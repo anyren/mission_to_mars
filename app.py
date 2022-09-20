1f872a5e49e077dc1e7d4518ce6ffa53e51e797e
anyren <anyren42@gmail.com>
2022-09-20 15:03:10 -0500

    mostly working code, a few todo's left

--- app.py
+++ app.py
@@ -1,44 +1,30 @@
 from flask import Flask, render_template, redirect
 import pymongo
-# from flask_pymongo import PyMongo
 import scrape
 
 app = Flask(__name__)
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
-# mongo = PyMongo(app)
 
-# Create connection variable
+# mongo setup
 conn = 'mongodb://localhost:27017'
-# Pass connection to the pymongo instance.
 client = pymongo.MongoClient(conn)
-# Connect to a database. Will create one if not already available.
 db = client.mars
 
 
 @app.route("/")
 def index():
-    # find one document from our mongo db and return it.
-    mars_facts = db.facts.find_one()
-    # pass that listing to render_template
-    return render_template("index.html", mars_facts=mars_facts)
-    
+    data = db.mars_data.find_one()
+    news_title = data['news_title']
+    news_body = data['news_body']
+    facts = data['mars_facts_html']
+    hemispheres = data['mars_hemispheres']
 
+    return render_template("index.html", mars_facts=facts, title=news_title, body=news_body, hemispheres=hemispheres)
+    
 
 @app.route("/scrape")
 def scraper():
-
-    # news_title, news_body = scrape.scrape_news()
-    # news_content =[news_title, news_body]
-    # db.news.update_one({},{"$set":news_content},upsert=True)
-
-    mars_facts_html = scrape.scrape_facts()
-    print(mars_facts_html)
-    db.facts.update_one({},{"$set":{mars_facts_html:mars_facts_html}}, upsert=True)
-
-    # hemisphere_image_urls = scrape.scrape_hemispheres()
-    # db.mars_hem.update_one({}, {"$set": hemisphere_image_urls}, upsert=True)
-
-    # return a message to our page so we know it was successful.
+    data_dict = scrape.scrape_mars()
+    db.mars_data.update_one({},{"$set":data_dict},upsert=True)
     return redirect("/", code=302)
 
 if __name__ == "__main__":
--- scrape.py
+++ scrape.py
@@ -1,51 +1,48 @@
 import requests
 import pandas as pd
-#from splinter import Browser
+from splinter import Browser
 from bs4 import BeautifulSoup
-#from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+def scrape_mars():
+    data_dict = {}
 
-# def scrape_news():
-#     # Scrape the Mars News Site and collect the latest News Title and Paragraph Text. 
-#     # Assign the text to variables that you can reference later.
-#     mns = 'https://redplanetscience.com'
-#     # Setup splinter
-#     executable_path = {'executable_path': ChromeDriverManager().install()}
-#     browser = Browser('chrome', **executable_path, headless=False)
-#     browser.visit(mns)
-#     html = browser.html
-#     soup = BeautifulSoup(html, 'html.parser')
-#     news_title = soup.find('div', class_='content_title').text
-#     news_body = soup.find('div', class_='article_teaser_body').text
-#     browser.quit()
-#     return(news_title, news_body)
+    # NEWS SCRAPE
+    mns = 'https://redplanetscience.com'
+    # Setup splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    browser.visit(mns)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    news_title = soup.find('div', class_='content_title').text
+    news_body = soup.find('div', class_='article_teaser_body').text
+    browser.quit()
 
-def scrape_facts():
-    # Visit the Mars Facts webpage and use Pandas to scrape the table containing facts about the planet 
-    # including diameter, mass, etc.
-    # Use Pandas to convert the data to a HTML table string.
+    data_dict['news_title'] = news_title
+    data_dict['news_body'] = news_body
+
+
+    # MARS FACTS SCRAPE
     mf = 'https://galaxyfacts-mars.com/'
     tables = pd.read_html(mf)
     mars_facts = tables[1]
-    mars_facts_html = mars_facts.to_html()
+    mars_facts_html = mars_facts.to_html(index=False, header=False)
     mars_facts_html.replace('\n', '')
-    return(mars_facts_html)
 
-# def scrape_hemisphere():
-#     # Visit the astrogeology site to obtain high-resolution images for each hemisphere of Mars.
-#     # You will need to click each of the links to the hemispheres in order to find the image URL to the 
-#     # full-resolution image.
-#     # Save the image URL string for the full resolution hemisphere image and the hemisphere title containing the 
-#     # hemisphere name. 
-#     # Use a Python dictionary to store the data using the keys img_url and title.
-#     # Append the dictionary with the image URL string and the hemisphere title to a list. This list will contain one 
-#     # dictionary for each hemisphere.
-#     mh = 'https://marshemispheres.com/cerberus.html'
-#     hemisphere_image_urls = [
-#     {"title": "Valles Marineris Hemisphere", "img_url": "https://marshemispheres.com/images/valles_marineris_enhanced.tif"},
-#     {"title": "Cerberus Hemisphere", "img_url": "https://marshemispheres.com/images/cerberus_enhanced.tif"},
-#     {"title": "Schiaparelli Hemisphere", "img_url": "https://marshemispheres.com/images/schiaparelli_enhanced.tif"},
-#     {"title": "Syrtis Major Hemisphere", "img_url": "https://marshemispheres.com/images/syrtis_major_enhanced.tif"}]
-#     return(hemisphere_image_urls)
+    data_dict['mars_facts_html'] = mars_facts_html
+
+
+    # HEMISPHERE SCRAPE
+    mh = 'https://marshemispheres.com/cerberus.html'
+
+    data_dict['mars_hemispheres']  = [
+    {"title": "Valles Marineris Hemisphere", "img_url": "https://marshemispheres.com/images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg"},
+    {"title": "Cerberus Hemisphere", "img_url": "https://marshemispheres.com/images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg"},
+    {"title": "Schiaparelli Hemisphere", "img_url": "https://marshemispheres.com/images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg"},
+    {"title": "Syrtis Major Hemisphere", "img_url": "https://marshemispheres.com/images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg"}
+    ]
+    return data_dict
+
 
 
     
